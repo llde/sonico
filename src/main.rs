fa56345d2e7a19b3e76f3ddc2a52b3d95d580ace
Lorenzo <lorenzofersteam@live.it>
2016-09-02 23:10:18 +0200

    external dependancyies. Require OpenSSL installed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate yup_oauth2 as oauth2;
+extern crate google_youtube3 as youtube3;
+use youtube3::{Result, Error};
+use std::default::Default;
+use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ApplicationSecret, MemoryStorage};
+use youtube3::YouTube;
+
 fn main() {
     println!("Hello, world!");
 }
